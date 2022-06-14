246a192423cc459ed0eefee8fb86240325ce557b
bisbalo <105565743+bisbalo@users.noreply.github.com>
2022-06-14 21:26:50 +0100

    a new data driven project eith example

---
+++ DatatDrivenWithExamples/Feature File/DataDrivenWithExamples.feature
@@ -0,0 +1,23 @@
+﻿Feature: DataDrivenWithExamples
+
+To be be able to enter different data for the same Scenario on the website
+	
+	
+	
+	
+	
+	Scenario Outline: Valid Registration
+	Given I navigate to the website "https://angularjs.realworld.io/#/"
+	And I click on sign up 
+	And I enter my username "<username>"
+	And I enter my email address "<email>"
+	And i enter my password "<password>"
+	When I click on the sign up button
+	Then I should be able to sign up successfully
+
+	Examples:
+	| username | email           | password |
+	| Ade      | ade@yahoo.com   | dodo1    |
+	| Meena    | meena@gmail.com | nana101  |
+	| Morry    | morry@yahoo.com | didi     |
+	| Bisi     | bisi@aol.com    | bisbis   |
---
+++ DatatDrivenWithExamples/Step Definition/DataSteps.cs
@@ -0,0 +1,68 @@
+﻿using DatatDrivenWithExamples.Page_Object;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace DatatDrivenWithExamples.Step_Definition
+{
+    [Binding]
+    public class DataSteps
+    {
+        DataRegistrationPage dataregistrationpage;
+
+        public DataSteps ()
+        {
+            dataregistrationpage = new DataRegistrationPage();
+
+
+        }
+
+        
+
+          
+      [Given(@"I navigate to the website ""(.*)""")]
+        public void GivenINavigateToTheWebsite(string url)
+
+        {
+            dataregistrationpage.NavigateToWebsite(url);
+        }
+        
+        [Given(@"I click on sign up")]
+        public void GivenIClickOnSignUp()
+        {
+            dataregistrationpage.ClickOnSignUp();
+        }
+        
+        [Given(@"I enter my username ""(.*)""")]
+        public void GivenIEnterMyUsername(string username)
+        {
+            dataregistrationpage.EnterUsername(username);
+        }
+        
+        [Given(@"I enter my email address ""(.*)""")]
+        public void GivenIEnterMyEmailAddress(string email)
+        {
+            dataregistrationpage.EnterEmailAddress(email);
+        }
+        
+        [Given(@"i enter my password ""(.*)""")]
+        public void GivenIEnterMyPassword(string password)
+        {
+            dataregistrationpage.EnterPassword(password);
+        }
+        
+        [When(@"I click on the sign up button")]
+        public void WhenIClickOnTheSignUpButton()
+        {
+            dataregistrationpage.ClickSignUpButton();
+        }
+        
+        [Then(@"I should be able to sign up successfully")]
+        public void ThenIShouldBeAbleToSignUpSuccessfully()
+        {
+            Thread.Sleep(3000);
+            Assert.That(dataregistrationpage.IsHomeDisplayed);
+        }
+    }
+}
